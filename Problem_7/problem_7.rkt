6644b7e10f30b67c6158eafb93bf8200561cebcd
Francesco <fran.gramano@mail.utoronto.ca>
2015-03-04 18:41:22 -0500

    Solves problem 7

---
+++ Problem_7/problem_7.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require math)
+(require racket/set)
+
+(provide comps)
+(define comps (set))
+
+(define (add n)
+  (set! comps (set-add comps n)))
+
+(define (sieve limit [cur 2] [countPrimes 0])
+      (if (set-member? comps cur)
+      (sieve limit (+ cur 1) countPrimes)
+      (if (prime? cur)
+          (if (>= (+ countPrimes 1) limit)
+              cur
+              (sieve limit (+ cur 1) (+ countPrimes 1)))
+          (begin
+            (add cur)
+            (sieve limit (+ cur 1) countPrimes)))))
