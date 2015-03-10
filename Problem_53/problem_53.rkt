9211596d4d380edbef86796b02373d5469b47d46
Francesco <fran.gramano@mail.utoronto.ca>
2015-03-10 00:08:57 -0400

    Solves problem 53

---
+++ Problem_53/problem_53.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require math)
+
+(define (count-comb [n 23] [r 10] [c 0])
+  (if (< r n)
+      (if (> (binomial n r) 1000000)
+          (count-comb n (+ r 1) (+ c 1))
+          (count-comb n (+ r 1) c))
+      (if (>= n 100)
+          c
+          (if (> (binomial n r) 1000000)
+              (count-comb (+ n 1) 1 (+ c 1))
+              (count-comb (+ n 1) 1 c)))))
