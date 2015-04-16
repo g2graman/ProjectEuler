b6afd6841dd19f399de2ecf3aed51d79e5405141
g2graman <fran.gramano@mail.utoronto.ca>
2015-04-15 20:01:09 -0400

    Fixes racket caching for travis CI

--- get_racket.sh
+++ get_racket.sh
@@ -2,6 +2,7 @@
 
 if ([ ! -e travis-racket ] && [ ! -d travis-racket ]); then
 	git clone https://github.com/greghendershott/travis-racket.git
-	cat travis-racket/install-racket.sh | bash
-	export PATH="${RACKET_DIR}/bin:${PATH}"
 fi
+
+cat travis-racket/install-racket.sh | bash
+export PATH="${RACKET_DIR}/bin:${PATH}"
