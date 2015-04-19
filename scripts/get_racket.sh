bd5a0a3598fee97a02eac2871e82f0a398a4754b
g2graman <fran.gramano@mail.utoronto.ca>
2015-04-19 06:20:01 -0400

    Reverts major quickfix of get_racket.sh, cleans build chain, and adds local aliasing to run_tests.sh

--- scripts/get_racket.sh
+++ scripts/get_racket.sh
@@ -19,35 +19,22 @@ if [ ! -e cache ] || [ ! -d cache ]; then
 	mkdir cache
 fi
 
+cd cache
 
 if [[ ! -e "$RACKET_DIR" ]] || [[ ! -d "$RACKET_DIR" ]]; then
-	mkdir $RACKET_DIR
-	cd $RACKET_DIR
-else
-	cd $RACKET_DIR
-fi
-
-INSTALL=$(ls | grep '^racket*.sh' | tr -d '[:blank:]')
-if [[ -z "$INSTALL" ]]; then
-	echo "Racket installation script not found, building."
-	
-	if ([ ! -e travis-racket ] || [ ! -d travis-racket ] \
-	|| [ ! -e travis-racket/install-racket.sh ] \
-	|| [ ! -f travis-racket/install-racket.sh ]); then
-		git clone https://github.com/greghendershott/travis-racket.git
+	INSTALL=$(ls | grep '^racket*.sh' | tr -d '[:blank:]')
+	if [[ -z "$INSTALL" ]]; then
+		echo "Racket installation script not found, building."
+		
+		if ([ ! -e travis-racket ] || [ ! -d travis-racket ] \
+		|| [ ! -e travis-racket/install-racket.sh ] \
+		|| [ ! -f travis-racket/install-racket.sh ]); then
+			git clone https://github.com/greghendershott/travis-racket.git
+		fi
+		bash < travis-racket/install-racket.sh
+	else
+		"./$INSTALL"
 	fi
-	bash < travis-racket/install-racket.sh
-else
-	"./$INSTALL"
 fi
-	
-which racket &>/dev/null
-ESTATUS=$?
-if [[ -n $ESTATUS ]]; then
-	echo "Adding racket to PATH"
-	export PATH="$PATH:$RACKET_DIR/bin"
-fi
-
-alias racket='$RACKET_DIR/bin/racket'
 
 cd "$START"
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,27 +5,33 @@
 #    ICDIFF=./bin/icdiff
 #fi
 
+if [[ -n "${RACKET_DIR-}" ]]; then
+	RACKET="$RACKET_DIR/bin/racket"
+else
+	RACKET=racket
+fi
+
 SELECTED_FIXTURES=$1
 if [ -z "${1-}" ]; then
 	for file in $(ls); do
-		if [ $(ls $file | grep answer.txt | wc -w) -gt 0 ]; then 
+		if [ $(ls "$file" | grep -c answer.txt) -gt 0 ]; then 
 			SELECTED_FIXTURES="$SELECTED_FIXTURES $file";
 		fi
 	done
 fi
 
-TEMPLATE=`cat test-template.rkt`
+TEMPLATE=$(cat test-template.rkt)
 ERR=0
 
 for fixture in $SELECTED_FIXTURES; do
 	echo "fixture: $fixture"
-	FILE_TO_TEST=$(ls $fixture | grep -P -x 'problem_[0-9]+\.rkt')
+	FILE_TO_TEST=$(ls "$fixture" | grep -P -x 'problem_[0-9]+\.rkt')
 	TEST_CASE=${TEMPLATE//"FILE-REPLACE"/$FILE_TO_TEST}
-	echo $FILE_TO_TEST
-	cd $fixture
-	RESULT=$(racket -e "$TEST_CASE")
-	ESTATUS=`echo $RESULT | cut -d " " -f 2`
-	OUTPUT=`echo $RESULT | cut -d " " -f 1`
+	echo "$FILE_TO_TEST"
+	cd "$fixture"
+	RESULT=$($RACKET -e "$TEST_CASE")
+	ESTATUS=$(echo "$RESULT" | cut -d " " -f 2)
+	#OUTPUT=$(echo "$RESULT" | cut -d " " -f 1)
 	#echo $OUTPUT
 	#echo $RESULT
 	cd ..
@@ -36,4 +42,4 @@ for fixture in $SELECTED_FIXTURES; do
 	if [ ! $ERR ] || [ ! $ESTATUS ]; then ERR=1; fi  #set error status
 done
 
-exit $ESTATUS
+exit "$ESTATUS"
